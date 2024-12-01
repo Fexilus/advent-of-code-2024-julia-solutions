0808fbd7f17ce1cef1663ebc3d2882024ca03ef9
Felix Augustsson <git.fexilus@gmail.com>
2024-12-01 06:27:25 +0100

    Initital solution to day 1

--- src/Day01.jl
+++ src/Day01.jl
@@ -17,26 +17,63 @@ ans1_file = joinpath(DATA_DIR, "day01.ans1")
 ans2_file = joinpath(DATA_DIR, "day01.ans2")
 
 function star1(input=stdin)
+    left_list = Int[]
+    right_list = Int[]
+
+    for line in eachline(input)
+        left, right = split(line; limit=2)
+
+        push!(left_list, parse(Int, left))
+        push!(right_list, parse(Int, right))
+    end
+
+    return sum(zip(sort(left_list), sort(right_list))) do (left, right)
+        abs(left - right)
+    end
 end
 
 hint1 = """
+    3   4
+    4   3
+    2   5
+    1   3
+    3   9
+    3   3
     """
 
 function test_hints_star1()
     @testset "Star 1 hints" begin
-        #@test star1(IOBuffer(hint1)) ==
+        @test star1(IOBuffer(hint1)) == 11
     end
 end
 
 function star2(input=stdin)
-end
+    left_list = Int[]
+    right_list = Int[]
 
-hint2 = """
-    """
+    for line in eachline(input)
+        left, right = split(line; limit=2)
+
+        push!(left_list, parse(Int, left))
+        push!(right_list, parse(Int, right))
+    end
+    right_occurances = Dict{Int, Int}()
+
+    for right in right_list
+        old_val = get!(right_occurances, right, 0)
+
+        right_occurances[right] = old_val + 1
+    end
+
+
+    return sum(left_list) do left
+        left * get!(right_occurances, left, 0)
+    end
+end
 
 function test_hints_star2()
     @testset "Star 2 hints" begin
-        #@test star2(IOBuffer(hint2)) ==
+        @test star2(IOBuffer(hint1)) == 31
     end
 end
 
