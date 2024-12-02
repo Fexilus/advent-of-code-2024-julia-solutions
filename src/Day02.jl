f0fd14cc3ec86dd628392b4a1c5fdedab4b39899
Felix Augustsson <git.fexilus@gmail.com>
2024-12-02 06:12:28 +0100

    Solve star 2.1

--- src/Day02.jl
+++ src/Day02.jl
@@ -1,6 +1,7 @@
 module Day02
 
 using Test
+using Logging
 
 using ..Utils: DATA_DIR
 
@@ -16,15 +17,37 @@ input_file = joinpath(DATA_DIR, "day02.input")
 ans1_file = joinpath(DATA_DIR, "day02.ans1")
 ans2_file = joinpath(DATA_DIR, "day02.ans2")
 
+parse_report(line) = parse.(Int, split(line))
+
 function star1(input=stdin)
+    return sum(eachline(input)) do line
+        report = parse_report(line)
+
+        dec = all(d -> (1 ≤ d ≤ 3), diff(report))
+        inc = all(d -> (-3 ≤ d ≤ -1), diff(report))
+
+        @debug "" report, dec, inc
+
+        if dec || inc
+            return 1
+        else
+            return 0
+        end
+    end
 end
 
 hint1 = """
+    7 6 4 2 1
+    1 2 7 8 9
+    9 7 6 2 1
+    1 3 2 4 5
+    8 6 4 4 1
+    1 3 6 7 9
     """
 
 function test_hints_star1()
     @testset "Star 1 hints" begin
-        #@test star1(IOBuffer(hint1)) ==
+        @test star1(IOBuffer(hint1)) == 2
     end
 end
 
