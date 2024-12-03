1f010e38989b7bf2388144b71fd970421b84debf
Felix Augustsson <git.fexilus@gmail.com>
2024-12-03 06:11:07 +0100

    Solve 3.1

--- src/Day03.jl
+++ src/Day03.jl
@@ -16,15 +16,28 @@ input_file = joinpath(DATA_DIR, "day03.input")
 ans1_file = joinpath(DATA_DIR, "day03.ans1")
 ans2_file = joinpath(DATA_DIR, "day03.ans2")
 
+mul_regex = r"mul\((\d{1,3}),(\d{1,3})\)"
+
 function star1(input=stdin)
+    s = 0
+    for line in eachline(input)
+        for regex_match in eachmatch(mul_regex, line)
+            num1 = parse(Int, regex_match.captures[1])
+            num2 = parse(Int, regex_match.captures[2])
+            s += num1 * num2
+        end
+    end
+
+    return s
 end
 
 hint1 = """
+    xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))
     """
 
 function test_hints_star1()
     @testset "Star 1 hints" begin
-        #@test star1(IOBuffer(hint1)) ==
+        @test star1(IOBuffer(hint1)) == 161
     end
 end
 
