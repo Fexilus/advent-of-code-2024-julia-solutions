f2afe9ec45a2decef6b78ce5932be84be4c30da6
Felix Augustsson <git.fexilus@gmail.com>
2024-12-03 06:34:09 +0100

    Solve 3.2

--- src/Day03.jl
+++ src/Day03.jl
@@ -41,15 +41,60 @@ function test_hints_star1()
     end
 end
 
+do_regex = r"do\(\)"
+dont_regex = r"don't\(\)"
+
 function star2(input=stdin)
+    s = 0
+    disabled = false
+    for line in eachline(input)
+        @info "$line"
+        index = 1
+        while index != length(line)
+            if disabled
+                next_do = match(do_regex, line, index)
+
+                if isnothing(next_do)
+                    @info "Found no more do"
+                    break
+                else
+                    @info "Found a do" next_do
+                    index = next_do.offset + length(next_do)
+                    disabled = false
+                end
+            else
+                next_mul = match(mul_regex, line, index)
+                next_dont = match(dont_regex, line, index)
+
+                if isnothing(next_mul)
+                    @info "Line done"
+                    break
+                elseif isnothing(next_dont) || next_mul.offset < next_dont.offset
+                    @info "Found mul first" next_mul
+                    num1 = parse(Int, next_mul.captures[1])
+                    num2 = parse(Int, next_mul.captures[2])
+                    s += num1 * num2
+
+                    index = next_mul.offset + length(next_mul)
+                else
+                    @info "Found don't first" next_dont
+                    index = next_dont.offset + length(next_dont)
+                    disabled = true
+                end
+            end
+        end
+    end
+
+    return s
 end
 
 hint2 = """
+    xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))
     """
 
 function test_hints_star2()
     @testset "Star 2 hints" begin
-        #@test star2(IOBuffer(hint2)) ==
+        @test star2(IOBuffer(hint2)) == 48
     end
 end
 
