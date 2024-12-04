5b357b50aa7ea53c54fda520af3e1c41416111bd
Felix Augustsson <git.fexilus@gmail.com>
2024-12-04 06:26:17 +0100

    Solved 4.1

--- src/Day04.jl
+++ src/Day04.jl
@@ -1,5 +1,6 @@
 module Day04
 
+using Base: Cartesian
 using Test
 
 using ..Utils: DATA_DIR
@@ -16,15 +17,57 @@ input_file = joinpath(DATA_DIR, "day04.input")
 ans1_file = joinpath(DATA_DIR, "day04.ans1")
 ans2_file = joinpath(DATA_DIR, "day04.ans2")
 
+parse_input(input) = stack(readlines(input); dims=1)
+
 function star1(input=stdin)
+    matrix = parse_input(input)
+
+    c = 0
+    for transformed_matrix in [matrix, rotl90(matrix), rot180(matrix), rotr90(matrix)]
+        for i in eachindex(IndexCartesian(), transformed_matrix)
+            k, l = Tuple(i)
+            if transformed_matrix[k, l] == 'X'
+                @debug "" transformed_matrix
+                if (k ≤ size(transformed_matrix, 2) - 3
+                    && transformed_matrix[k + 1, l] == 'M'
+                    && transformed_matrix[k + 2, l] == 'A'
+                    && transformed_matrix[k + 3, l] == 'S'
+                   )
+
+                    c += 1
+                end
+                if (k ≤ size(transformed_matrix, 2) - 3
+                    && l ≤ size(transformed_matrix, 1) - 3
+                    && transformed_matrix[k + 1, l + 1] == 'M'
+                    && transformed_matrix[k + 2, l + 2] == 'A'
+                    && transformed_matrix[k + 3, l + 3] == 'S'
+                   )
+
+                    c += 1
+                end
+            end
+        end
+    end
+
+    return c
 end
 
 hint1 = """
+    MMMSXXMASM
+    MSAMXMSMSA
+    AMXSXMAAMM
+    MSAMASMSMX
+    XMASAMXAMM
+    XXAMMXXAMA
+    SMSMSASXSS
+    SAXAMASAAA
+    MAMMMXMMMM
+    MXMXAXMASX
     """
 
 function test_hints_star1()
     @testset "Star 1 hints" begin
-        #@test star1(IOBuffer(hint1)) ==
+        @test star1(IOBuffer(hint1)) == 18
     end
 end
 
