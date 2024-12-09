73c6bdf95f430b14b443cd9e4a722d6ba387a257
Felix Augustsson <git.fexilus@gmail.com>
2024-12-09 06:36:27 +0100

    Solve 9.1

--- src/Day09.jl
+++ src/Day09.jl
@@ -17,14 +17,71 @@ ans1_file = joinpath(DATA_DIR, "day09.ans1")
 ans2_file = joinpath(DATA_DIR, "day09.ans2")
 
 function star1(input=stdin)
+    line = readline(input)
+
+    cur_id = 0
+    cur_pos = 0
+   
+    cur_tail = Int[]
+    tail_index = length(line) + 2
+    if iseven(tail_index)
+        tail_index -= 1
+    end
+
+    s = 0
+    for m in eachmatch(r"(\d)(\d)", line)
+
+        if m.offset == tail_index
+            break
+        end
+
+        block_size = parse(Int, m.captures[1])
+        space_size = parse(Int, m.captures[2])
+
+        new_pos = cur_pos + block_size
+
+        for pos in cur_pos:(new_pos - 1)
+            s += pos * cur_id
+        end
+
+        cur_pos = new_pos
+
+        new_pos = cur_pos + space_size
+        
+        for pos in cur_pos:(new_pos - 1)
+            while isempty(cur_tail)
+                tail_index -= 2
+                tail_id = tail_index ÷ 2
+                
+                tail_block_size = parse(Int, line[tail_index])
+
+                for i in 1:tail_block_size
+                    push!(cur_tail, tail_id)
+                end  
+            end
+
+            s += pos * pop!(cur_tail)
+        end
+
+        cur_pos = new_pos
+
+        cur_id += 1
+    end
+
+    new_pos = cur_pos + length(cur_tail)
+
+    for pos in cur_pos:(new_pos - 1)
+        s += pos * pop!(cur_tail)
+    end
+
+    return s
 end
 
-hint1 = """
-    """
+hint1 = "2333133121414131402"
 
 function test_hints_star1()
     @testset "Star 1 hints" begin
-        #@test star1(IOBuffer(hint1)) ==
+        @test star1(IOBuffer(hint1)) == 1928
     end
 end
 
