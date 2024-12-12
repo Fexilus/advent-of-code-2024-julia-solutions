37eda8596be4d43a77393ef8ca12575f7e8023e4
Felix Augustsson <git.fexilus@gmail.com>
2024-12-12 07:23:56 +0100

    Set up 12.2 testing

--- src/Day12.jl
+++ src/Day12.jl
@@ -129,14 +129,81 @@ function test_hints_star1()
 end
 
 function star2(input=stdin)
+    lot_matrix = parse_input(input)
+
+    unused_ids = Iterators.Stateful(Iterators.countfrom())
+    lot_ids = Dict{CartesianIndex, Int}()
+    merged_lots = Dict{Int, Int}()
+
+    for i in eachindex(IndexCartesian(), lot_matrix)
+        lot_plant = lot_matrix[i]
+        neighbor_i = get_neighbor_indices(i, lot_matrix)
+
+        neighbors_in_region = filter(i -> lot_matrix[i] == lot_plant, neighbor_i)
+        id_neighbors_in_region = filter(i -> i ∈ keys(lot_ids), neighbors_in_region)
+
+        # Figure out the id of the lot
+        if !isempty(id_neighbors_in_region)
+            main_id = lot_ids[first(id_neighbors_in_region)]
+
+            while main_id ∈ keys(merged_lots)
+                main_id = merged_lots[main_id]
+            end
+
+            # Manage merges of regions
+            for other_region_neighbor in Iterators.drop(id_neighbors_in_region, 1)
+                other_id = lot_ids[other_region_neighbor]
+                
+                while other_id ∈ keys(merged_lots)
+                    other_id = merged_lots[other_id]
+                end
+
+                if other_id != main_id
+                    merged_lots[other_id] = main_id
+                end
+            end
+        else
+            main_id = popfirst!(unused_ids)
+        end
+
+        lot_ids[i] = main_id
+    end
+
+    for (index, id) in lot_ids
+        actual_id = merged_lots[id]
+
+        while actual_id ∈ keys(merged_lots)
+            actual_id = merged_lots[actual_id]
+        end
+
+        lot_ids[index] = actual_id
+    end
 end
 
-hint2 = """
+hint4 = """
+    EEEEE
+    EXXXX
+    EEEEE
+    EXXXX
+    EEEEE
+    """
+
+hint5 = """
+    AAAAAA
+    AAABBA
+    AAABBA
+    ABBAAA
+    ABBAAA
+    AAAAAA
     """
 
 function test_hints_star2()
     @testset "Star 2 hints" begin
-        #@test star2(IOBuffer(hint2)) ==
+        @test star2(IOBuffer(hint1)) == 80
+        @test star2(IOBuffer(hint2)) == 436
+        @test star2(IOBuffer(hint4)) == 236
+        @test star2(IOBuffer(hint5)) == 368
+        @test star2(IOBuffer(hint3)) == 1206
     end
 end
 
