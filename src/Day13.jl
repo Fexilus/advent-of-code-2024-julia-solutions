14b872baf17722eb1fc9aef6310b4a8c25a9c1f8
Felix Augustsson <git.fexilus@gmail.com>
2024-12-13 06:46:25 +0100

    Solve 13.2

--- src/Day13.jl
+++ src/Day13.jl
@@ -17,18 +17,25 @@ input_file = joinpath(DATA_DIR, "day13.input")
 ans1_file = joinpath(DATA_DIR, "day13.ans1")
 ans2_file = joinpath(DATA_DIR, "day13.ans2")
 
-function parse_input(input)
+function parse_input(input; machine_error=true)
     return map(Iterators.partition(eachline(input), 4)) do lines
         a_move = parse.(Int, match(r"Button A: X\+(\d+), Y\+(\d+)", lines[1]).captures)
         b_move = parse.(Int, match(r"Button B: X\+(\d+), Y\+(\d+)", lines[2]).captures)
         goal = parse.(Int, match(r"Prize: X=(\d+), Y=(\d+)", lines[3]).captures)
+
+        if machine_error
+            goal_x = 10000000000000 + goal[1]
+            goal_y = 10000000000000 + goal[2]
+
+            goal = [goal_x, goal_y]
+        end
 
         return a_move, b_move, goal
     end
 end
 
 function star1(input=stdin)
-    return sum(parse_input(input)) do (a_move, b_move, goal)
+    return sum(parse_input(input; machine_error=false)) do (a_move, b_move, goal)
         mat = stack((a_move, b_move); dims=2)
 
         det_int = mat[1, 1] * mat[2, 2] - mat[2, 1] * mat[1, 2]
@@ -74,6 +81,25 @@ function test_hints_star1()
 end
 
 function star2(input=stdin)
+    return sum(parse_input(input)) do (a_move, b_move, goal)
+        mat = stack((a_move, b_move); dims=2)
+
+        det_int = mat[1, 1] * mat[2, 2] - mat[2, 1] * mat[1, 2]
+        adjugate_int = [mat[2, 2]  -mat[1, 2]
+                        -mat[2, 1] mat[1, 1]]
+
+        if all(rem.(adjugate_int * goal, det_int) .== 0)
+            @info "Machine possible" a_move, b_move, goal
+            required_moves = adjugate_int * goal .÷ det_int
+            
+            cost = required_moves ⋅ [3, 1]
+            @debug "Cost" cost
+
+            return cost
+        else
+            return 0
+        end
+    end
 end
 
 hint2 = """
