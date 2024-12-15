4013ed9cb5e28a0411b7eaa6d5822f6f67d8ad83
Felix Augustsson <git.fexilus@gmail.com>
2024-12-15 20:20:13 +0100

    Solve 12.2

--- src/Day12.jl
+++ src/Day12.jl
@@ -2,6 +2,8 @@ module Day12
 
 using Test
 
+using DataStructures: OrderedDict
+
 using ..Utils: DATA_DIR
 
 export input_file
@@ -129,54 +131,76 @@ function test_hints_star1()
 end
 
 function star2(input=stdin)
-    lot_matrix = parse_input(input)
-
     unused_ids = Iterators.Stateful(Iterators.countfrom())
-    lot_ids = Dict{CartesianIndex, Int}()
-    merged_lots = Dict{Int, Int}()
+    prev_ranges = Dict{Char, OrderedDict{UnitRange{Int}, Int}}()
+    measurements = Dict{Int, Vector{Int}}()
 
-    for i in eachindex(IndexCartesian(), lot_matrix)
-        lot_plant = lot_matrix[i]
-        neighbor_i = get_neighbor_indices(i, lot_matrix)
+    for line in eachline(input)
+        cur_ranges = Dict{Char, OrderedDict{UnitRange{Int}, Int}}()
+        id_mappings = Dict{Int, Int}()
 
-        neighbors_in_region = filter(i -> lot_matrix[i] == lot_plant, neighbor_i)
-        id_neighbors_in_region = filter(i -> i ∈ keys(lot_ids), neighbors_in_region)
+        for m in eachmatch(r"((\w)\g{-1}*)", line)
+            region_range = m.offset:(m.offset + length(first(m)) - 1)
+            region_char = only(m.captures[2])
+            region_id = missing
 
-        # Figure out the id of the lot
-        if !isempty(id_neighbors_in_region)
-            main_id = lot_ids[first(id_neighbors_in_region)]
+            @debug "" region_range, region_char
 
-            while main_id ∈ keys(merged_lots)
-                main_id = merged_lots[main_id]
+            new_sides = 4
+
+            for (prev_range, prev_id) in get(prev_ranges, region_char, Dict())
+                if !isdisjoint(prev_range, region_range)
+                    @debug "Overlaps with" prev_range
+
+                    # Resolve if previous id has been remapped this line
+                    prev_id = get(id_mappings, prev_id, prev_id)
+
+                    if ismissing(region_id)
+                        # Inherit id from first overlapping region
+                        region_id = prev_id
+                    elseif prev_id != region_id
+                        # Remap later overlapping regions
+                        id_mappings[prev_id] = region_id
+                    end
+
+                    if first(prev_range) == first(region_range)
+                        new_sides -= 2
+                    end
+
+                    if last(prev_range) == last(region_range)
+                        new_sides -= 2
+                    end
+                end
             end
 
-            # Manage merges of regions
-            for other_region_neighbor in Iterators.drop(id_neighbors_in_region, 1)
-                other_id = lot_ids[other_region_neighbor]
-                
-                while other_id ∈ keys(merged_lots)
-                    other_id = merged_lots[other_id]
-                end
-
-                if other_id != main_id
-                    merged_lots[other_id] = main_id
-                end
+            # Test if region was unconnected to previous regions
+            if ismissing(region_id)
+                region_id = popfirst!(unused_ids)
             end
-        else
-            main_id = popfirst!(unused_ids)
+
+            # Record new changes to measurements of area
+            region_measurements = get!(measurements, region_id, [0, 0])
+            # Area
+            region_measurements[1] += length(region_range)
+            # Sides
+            region_measurements[2] += new_sides
+
+            # Add this region slice to new dict of slices
+            plot_type_dict = get!(cur_ranges, region_char, OrderedDict{UnitRange, Int}())
+            plot_type_dict[region_range] = region_id
         end
 
-        lot_ids[i] = main_id
+        # Consolidate old id measurements
+        for (old_id, new_id) in id_mappings
+            old_measurements = pop!(measurements, old_id)
+            measurements[new_id] += old_measurements
+        end
+
+        prev_ranges = cur_ranges
     end
 
-    for (index, id) in lot_ids
-        actual_id = merged_lots[id]
-
-        while actual_id ∈ keys(merged_lots)
-            actual_id = merged_lots[actual_id]
-        end
-
-        lot_ids[index] = actual_id
+    return sum(values(measurements)) do (area, sides)
+        return area * sides
     end
 end
 
