d2702807497e4516fbc1960a77678210cb5ee5c8
Felix Augustsson <git.fexilus@gmail.com>
2024-12-16 07:07:49 +0100

    Solve 16.2

--- src/Day16.jl
+++ src/Day16.jl
@@ -37,7 +37,7 @@ function parse_input(input)
     return walls, start, exit
 end
 
-State = Tuple{CartesianIndex{2}, Symbol}
+const State = Tuple{CartesianIndex{2}, Symbol}
 
 function star1(input=stdin)
     walls, start, exit = parse_input(input)
@@ -102,6 +102,88 @@ function test_hints_star1()
 end
 
 function star2(input=stdin)
+    walls, start, exit = parse_input(input)
+
+    visited = Set{State}()
+    unexplored = PriorityQueue((start, :right) => 0)
+    best_prev_spots = Dict{State, Vector{State}}()
+    best_score = missing
+    exit_states = Set{State}()
+
+    while !isempty(unexplored)
+        (pos, dir), score = dequeue_pair!(unexplored)
+
+        @debug "At" pos
+
+        if pos == exit
+            if ismissing(best_score)
+                best_score = score
+            elseif score > best_score
+                break
+            end
+
+            push!(exit_states, (pos, dir))
+
+            continue
+        elseif walls[pos]
+            continue
+        end
+
+        if dir === :up
+            forward_pos = pos + CartesianIndex(-1, 0)
+            clockwise_dir = :right
+            counterclockwise_dir = :left
+        elseif dir === :right
+            forward_pos = pos + CartesianIndex(0, 1)
+            clockwise_dir = :down
+            counterclockwise_dir = :up
+        elseif dir === :down
+            forward_pos = pos + CartesianIndex(1, 0)
+            clockwise_dir = :left
+            counterclockwise_dir = :right
+        elseif dir === :left
+            forward_pos = pos + CartesianIndex(0, -1)
+            clockwise_dir = :up
+            counterclockwise_dir = :down
+        end
+
+        potential_states = [((forward_pos, dir), 1),
+                            ((pos, clockwise_dir), 1000),
+                            ((pos, counterclockwise_dir), 1000)]
+
+        for (state, extra_score) in potential_states
+            if state ∉ visited
+                prev_score = get(unexplored, state, Inf)
+                new_score = score + extra_score
+
+                unexplored[state] = min(new_score, prev_score)
+
+                if new_score < prev_score
+                    best_prev_spots[state] = [(pos, dir)]
+                elseif new_score == prev_score
+                    push!(best_prev_spots[state], (pos, dir))
+                end
+            end
+
+        end
+
+        push!(visited, (pos, dir))
+    end
+
+    on_best_path = Set{CartesianIndex{2}}()
+    untraced_states = collect(exit_states)
+
+    while !isempty(untraced_states)
+        pos, dir = pop!(untraced_states)
+
+        if pos != start
+            append!(untraced_states, best_prev_spots[(pos, dir)])
+        end
+
+        push!(on_best_path, pos)
+    end
+
+    return length(on_best_path)
 end
 
 hint2 = """
