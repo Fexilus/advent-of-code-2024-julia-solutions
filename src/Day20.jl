d50622521dc7a8bf3a0be99f345ab1f642e41c50
Felix Augustsson <git.fexilus@gmail.com>
2024-12-20 06:58:49 +0100

    Solve 20.2

--- src/Day20.jl
+++ src/Day20.jl
@@ -139,7 +139,87 @@ function test_hints_star1()
     end
 end
 
-function star2(input=stdin)
+function find_unexplored_n_neighbors(map, pos, visited, n)
+    steps = [CartesianIndex(-1, 0),
+             CartesianIndex(0, 1),
+             CartesianIndex(1, 0),
+             CartesianIndex(0, -1)]
+
+    neighbor_distances = Dict{CartesianIndex{2}, Int}()
+    last_neighbors = Set([pos])
+    lastlast_neighbors = Set()
+
+    for i in 1:n
+        new_neighbors = Set{CartesianIndex{2}}()
+
+        for prev_neighbor in last_neighbors
+            for step in steps
+                new_neighbor = prev_neighbor + step
+
+                if new_neighbor ∉ last_neighbors
+                    push!(new_neighbors, new_neighbor)
+                end
+
+                if (new_neighbor ∉ keys(neighbor_distances)
+                    && checkbounds(Bool, map, new_neighbor)
+                    && !map[new_neighbor]
+                    && new_neighbor ∉ visited)
+
+                    neighbor_distances[new_neighbor] = i
+                end
+            end
+        end
+
+        lastlast_neighbors = last_neighbors
+        last_neighbors = new_neighbors
+    end
+
+    return neighbor_distances
+end
+
+function star2(input=stdin; minimum_savings=100)
+    walls, start, exit = parse_input(input)
+  
+    good_cheats = Dict{Tuple{CartesianIndex, CartesianIndex}, Int}()
+
+    distance_to_exit = find_distances(walls, exit)
+    time_to_beat = distance_to_exit[start]
+
+    visited = Set{CartesianIndex{2}}()
+
+    cur_pos = start
+    distance = 0
+    while cur_pos != exit
+        push!(visited, cur_pos) 
+
+        cheat_candidates = find_unexplored_n_neighbors(walls, cur_pos, visited, 20)
+
+        @debug "" cheat_candidates
+
+        for (cheat_pos, steps) in cheat_candidates
+            cheat_time = distance + distance_to_exit[cheat_pos] + steps
+            savings = time_to_beat - cheat_time
+
+            if savings ≥ minimum_savings
+                good_cheats[(cur_pos, cheat_pos)] = savings
+            end
+        end
+
+        # Go to next position
+        distance += 1
+
+        next_pos_cand = find_unexplored_neighbors(walls, cur_pos, visited)
+
+        if isempty(next_pos_cand)
+            cur_pos = nothing
+        elseif length(next_pos_cand) == 1
+            cur_pos = only(next_pos_cand)
+        else
+            error("Map is not one path")
+        end
+    end
+
+    return length(good_cheats)
 end
 
 hint2 = """
